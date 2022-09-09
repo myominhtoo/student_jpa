415db10a293d3659eb5ce840ff0bbc97fea274bd
Myo Min Htoo <myominhtoo2003@gmail.com>
2022-09-09 18:09:42 +0630

    added service for Student

--- src/main/resources/views/src/app/services/student/StudentService.ts
+++ src/main/resources/views/src/app/services/student/StudentService.ts
@@ -1,6 +1,35 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Student } from "src/app/models/Student";
 
 @Injectable({
     providedIn : "root"
 })
-export default class StudentService{}
+export default class StudentService{
+
+    private  BASE_URL = "http://localhost:8080/api";
+
+    constructor( private httpClient : HttpClient ){}
+
+    getStudents() : Observable<Student[]> {
+       return  this.httpClient.get<Student[]>(`${this.BASE_URL}/students`);
+    }
+
+    getStudent( studentId : string ) : Observable<Student> {
+        return this.httpClient.get<Student>(`${this.BASE_URL}/students/${studentId}`);
+    }
+
+    addStudent( student : Student ) : Observable<Response> {
+        return this.httpClient.post<Response>( `${this.BASE_URL}/students` , student );
+    }
+
+    deleteStudent( studentId : string ) : Observable<Response>{
+        return this.httpClient.delete<Response>(`${this.BASE_URL}/students/${studentId}`);
+    }
+
+    updateStudent( student : Student ) : Observable<Response> {
+        return this.httpClient.put<Response>( `${this.BASE_URL}/students/${student.id}` , student );
+    }
+
+}
