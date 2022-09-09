1ccccf3b2bf2772e50201be89522086d6c7656f5
Myo Min Htoo <myominhtoo2003@gmail.com>
2022-09-09 18:02:47 +0630

    added service for User in views

--- src/main/resources/views/src/app/services/user/UserService.ts
+++ src/main/resources/views/src/app/services/user/UserService.ts
@@ -1,6 +1,35 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { User } from "src/app/models/User";
 
 @Injectable({
     providedIn : "root"
 })
-export default class UserService{}
+export default class UserService{
+
+    private BASE_URL = "http://localhost:8080/api";
+
+    constructor( private httpClient : HttpClient ){}
+
+    getUsers() : Observable<User[]> {
+        return this.httpClient.get<User[]>(`${this.BASE_URL}/users`);
+    }
+
+    getUser( userId : string ) : Observable<User>{
+        return this.httpClient.get<User>(`${this.BASE_URL}/users/${userId}`)
+    }
+
+    addUser( user : User ) : Observable<Response>{
+        return this.httpClient.post<Response>(`${this.BASE_URL}/users` , user );
+    }
+
+    deleteUser( userId : string ) : Observable<Response>{
+        return this.httpClient.delete<Response>(`${this.BASE_URL}/users/${userId}`);
+    }
+
+    updateUser( user : User ) : Observable<Response>{
+        return this.httpClient.put<Response>( `${this.BASE_URL}/users/${user.id}` , user );
+    }
+
+}
