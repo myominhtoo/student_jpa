028bfed8d0323ee07cf0639c2c21c2480a490397
Myo Min Htoo <myominhtoo2003@gmail.com>
2022-09-11 13:15:11 +0630

    added deleting in students in views

--- src/main/resources/views/src/app/components/student/students.component.ts
+++ src/main/resources/views/src/app/components/student/students.component.ts
@@ -2,6 +2,7 @@ import { Component , OnInit } from '@angular/core';
 import { Status } from 'src/app/models/Status';
 import { Student } from 'src/app/models/Student';
 import StudentService from 'src/app/services/student/StudentService';
+import swal from 'sweetalert';
 
 @Component({
     selector : 'students',
@@ -43,6 +44,28 @@ export class StudentsComponent implements OnInit {
 
             }
         })
+    }
+
+    handleDeleteStudent( studentId : string ) : void {
+        swal({
+            text : `Are you sure to delete ${studentId} course ? `,
+            icon : 'warning',
+            buttons : ['No','Yes'],
+        }).then( isYes => {
+            if(isYes){
+                this.studentService.deleteStudent( studentId )
+                .subscribe({
+                    next : ( res ) => {
+                        swal({
+                            text : res.msg,
+                            icon : res.ok ? 'success' : 'warning'
+                        });
+                        if( res.ok ) this.fetchStudents();
+                    },
+                    error : ( e ) => console.log( e )
+                });
+            }
+        });
     }
 
 }
